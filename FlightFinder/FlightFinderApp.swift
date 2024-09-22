2c4d97c22a494fc4f12349cad935912d57d12d0b
Mason Dierkes <mjdierkes@msn.com>
2024-09-22 16:40:31 -0500

    Initial Commit

---
+++ FlightFinder/FlightFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightFinderApp.swift
+//  FlightFinder
+//
+//  Created by Mason Dierkes on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FlightFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
